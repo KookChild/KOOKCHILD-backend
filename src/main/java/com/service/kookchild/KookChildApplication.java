69215c34f090ff30c4733e51eab2c181e3565c61
KSEOP <kts9335@naver.com>
2023-09-06 16:33:33 +0900

    init

---
+++ src/main/java/com/service/kookchild/KookChildApplication.java
@@ -0,0 +1,13 @@
+package com.service.kookchild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KookChildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KookChildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/kookchild/KookChildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.kookchild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KookChildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
