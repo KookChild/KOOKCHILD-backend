25849a23855e74ea7fd60c43a3cd121af69ec716
yoojung.choi <green980611@naver.com>
2023-09-12 09:31:57 +0900

    refactor: add EnableJpaAuditing

--- src/main/java/com/service/kookchild/KookChildApplication.java
+++ src/main/java/com/service/kookchild/KookChildApplication.java
@@ -2,9 +2,11 @@ package com.service.kookchild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KookChildApplication {
 
 	public static void main(String[] args) {
