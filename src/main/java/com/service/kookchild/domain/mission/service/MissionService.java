b114553db38acf261b66b9cec8e68f6e60dbed21
Dain Yoon <yoondain99@naver.com>
2023-09-12 12:48:49 +0900

    feat: 미션 등록 missioncreate

--- src/main/java/com/service/kookchild/domain/mission/service/MissionService.java
+++ src/main/java/com/service/kookchild/domain/mission/service/MissionService.java
@@ -1,19 +1,16 @@
 package com.service.kookchild.domain.mission.service;
 
 
+import com.service.kookchild.domain.mission.dto.MissionCreateDTO;
 import org.springframework.stereotype.Service;
 import com.service.kookchild.domain.mission.domain.Mission;
 
 
 @Service
 public interface MissionService {
-	
-	
-	void saveMission(Mission m); // 미션 만들어서 테이블 저장
-	
-	//jpa repo에서 자동 제공
-	//List<Mission> showAllMission(); // 모든 미션 보이게 하는
-	
+
+
+	void saveMission(MissionCreateDTO m, String email);
 
 
 }
